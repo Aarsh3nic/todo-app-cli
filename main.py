3a32703f5d065615c40d6685054324c37e44e9ca
Aarsh Patel <aarshpatel333@gmail.com>
2025-02-09 12:34:46 -0500

    Modified: show Added : Edit and Complete feature

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 todos = []
 
+def display(arr):
+    print("Here's the list:")
+    for task in enumerate(arr, start=1):
+        print(f" {task[0]}| {task[1].title()}")
+
 while True:
-    user_action = input("Type add, show or exit:").strip()
+    user_action = input("Type add, show, edit or exit:").strip()
     match user_action:
         case "add":
             todo = input("Enter to-do:")
             todos.append(todo)
-        case "show":
-            for task in todos:
-                print(task.title())
+        case "show" | "display":
+            display(todos)
+        case "edit":
+            display(todos)
+            num_task = int(input("What task you want to edit (type number): "))
+            todos[num_task-1] = input("Enter new task: ")
+            print("Modified!")
+
+        case "complete":
+            display(todos)
+            num_task = int(input("What task you want to mark as complete (type number): "))
+            todos.pop(num_task-1)
+            print("Modified!")
+            display(todos)
+
+
         case "exit":
             break
         case _:
@@ -17,3 +35,7 @@ while True:
 
 print("Bye")
 
+def display(arr):
+    print("Here's the list:")
+    for task in enumerate(arr, start=1):
+        print(f" {task[0]}| {task[1].title()}")
